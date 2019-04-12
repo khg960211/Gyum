469923ce8027519a032328c85b9acd725af68eea
Gyum <khg960211@naver.com>
2019-04-12 11:23:29 +0900

    거듭 제곱 빠르게 계산하기 알고리즘 연습

---
+++ codeit/알고리즘/알고리즘 연습 Level 1/거듭 제곱 빠르게 계산하기/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# 내가 짠 코드
+
+def power(x, y):
+    # 코드를 작성하세요.
+    if y < 2:
+        return x * y
+
+    result = power(x, y // 2)
+    
+    if y % 2 == 0:
+        return result * result
+    else:
+        return result * result * x
+
+# 테스트
+print(power(3, 5))
+print(power(5, 6))
+print(power(7, 9))
---
+++ codeit/알고리즘/알고리즘 연습 Level 1/거듭 제곱 빠르게 계산하기/main2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 해설
+
+def power(x, y):
+    if y == 0:
+        return 1
+
+    # 계산을 한 번만 하기 위해서 변수에 저장
+    subresult = power(x, y // 2)
+
+    # 문제를 최대한 똑같은 크기의 문제 두 개로 나눠준다 (짝수, 홀수 경우 따로)
+    if y % 2 == 0:
+        return subresult * subresult
+    else:
+        return x * subresult * subresult
+
+
+# 테스트
+print(power(3, 5))
+print(power(5, 6))
+print(power(7, 9))
