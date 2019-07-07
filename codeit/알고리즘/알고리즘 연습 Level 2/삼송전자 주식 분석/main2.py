8f6dfb074a12db0e79c4f90c0feed98259ae006e
khg960211 <khg960211@naver.com>
2019-07-07 20:07:15 +0900

    알고리즘 연습

--- codeit/알고리즘/알고리즘 연습 Level 2/삼송전자 주식 분석/main2.py
+++ codeit/알고리즘/알고리즘 연습 Level 2/삼송전자 주식 분석/main2.py
@@ -2,7 +2,21 @@
 # 시간복잡도 O(n)
 
 def max_profit(stock_list):
-    # 코드를 작성하세요.
+    # 현재까지의 최대 수익
+    max_profit_so_far = stock_list[1] - stock_list[0]
+
+    # 현재까지의 최소 주식 가격
+    min_so_far = min(stock_list[0], stock_list[1])
+
+    # 주식 가격을 하나씩 확인한다
+    for i in range(2, len(stock_list)):
+        # 현재 파는 것이 좋은지 확인한다
+        max_profit_so_far = max(max_profit_so_far, stock_list[i] - min_so_far)
+
+        # 현재 주식 가격이 최솟값인지 확인한다
+        min_so_far = min(min_so_far, stock_list[i])
+
+    return max_profit_so_far
 
 
 # 테스트
