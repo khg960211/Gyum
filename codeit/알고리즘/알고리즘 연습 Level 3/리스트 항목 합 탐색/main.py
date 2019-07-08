3e4b8e6edff81342380cf1d6539a3cce52aad09a
khg960211 <khg960211@naver.com>
2019-07-08 15:11:51 +0900

    리스트 항목 합 탐색

---
+++ codeit/알고리즘/알고리즘 연습 Level 3/리스트 항목 합 탐색/main.py
@@ -0,0 +1,13 @@
+# Brute Force 방법
+# 인풋 리스트의 길이가 n이라고 할 때 시간 복잡도는 O(n^2)
+def sum_in_list(search_sum, sorted_list):
+    # 코드를 쓰세요
+    for i in range(len(sorted_list)-1):
+        for j in range(i, len(sorted_list)-1):
+            if (search_sum == sorted_list[i] + sorted_list[j]):
+                return True
+    return False
+
+
+print(sum_in_list(15, [1, 2, 5, 6, 7, 9, 11]))
+print(sum_in_list(15, [1, 2, 5, 7, 9, 11]))
---
+++ codeit/알고리즘/알고리즘 연습 Level 3/리스트 항목 합 탐색/main2.py
@@ -0,0 +1,17 @@
+def sum_in_list(search_sum, sorted_list):
+    low = 0
+    high = len(sorted_list) - 1
+    while low < high:
+        if sorted_list[low] + sorted_list[high] == search_sum:
+            return True
+        elif sorted_list[low] + sorted_list[high] < search_sum:
+            low += 1
+        else:
+            high -= 1
+
+    return False
+
+
+
+print(sum_in_list(15, [1, 2, 5, 6, 7, 9, 11]))
+print(sum_in_list(15, [1, 2, 5, 7, 9, 11]))
